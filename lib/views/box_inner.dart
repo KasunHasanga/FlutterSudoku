a50aa817c05ef95c2850942d88bb4a7d66c78846
KasunHasanga <kasunhasanga96@gmail.com>
2022-07-23 00:00:50 +0530

    generating sudoku puzzel

---
+++ lib/views/box_inner.dart
@@ -0,0 +1,59 @@
+import 'package:sudoku_puzzle/views/block_char.dart';
+
+class BoxInner{
+  late int index;
+  List<BlockChar> blockChars =List<BlockChar>.from([]);
+
+  BoxInner(this.index,this.blockChars);
+
+  //declare method use
+setFocus(int index, Direction direction){
+  List<BlockChar> temp;
+  if(direction ==Direction.horizontal){
+    temp =blockChars.where((element) => element.index! ~/ 3 ==index ~/3).toList();
+  }else{
+    temp =blockChars.where((element) => element.index! % 3 ==index %3).toList();
+  }
+
+  temp.forEach((element) {
+    element.isFocus =true;
+  });
+
+
+}
+
+  setExistValue(int index,int indexBox,String textInput,Direction direction){
+  List<BlockChar> temp;
+  if(direction ==Direction.horizontal){
+    temp =blockChars.where((element) => element.index! ~/ 3 ==index ~/3).toList();
+  }else{
+    temp =blockChars.where((element) => element.index! % 3 ==index %3).toList();
+  }
+  
+  if (this.index ==indexBox){
+    List<BlockChar> blockCharsBox =blockChars.where((element) => element.text==textInput).toList();
+    
+    if(blockCharsBox.length==1 && temp.isEmpty) blockCharsBox.clear();
+    
+    temp.addAll(blockCharsBox);
+  }
+  temp.where((element) => element.text==textInput).forEach((element) {
+    element.isExist =false;
+  });
+  }
+
+  clearFocus(){
+  blockChars.forEach((element) {
+    element.isExist=false;
+  });
+  }
+
+  clearExist(){
+  blockChars.forEach((element) {
+    element.isFocus=false;
+  });
+  }
+
+}
+
+enum Direction {horizontal,vertical}
--- lib/views/sudoku_home_page.dart
+++ lib/views/sudoku_home_page.dart
@@ -1,5 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:quiver/iterables.dart';
+import 'package:sudoku_puzzle/views/block_char.dart';
+import 'package:sudoku_solver_generator/sudoku_solver_generator.dart';
+import 'package:sudoku_puzzle/views/box_inner.dart';
+import 'package:sudoku_puzzle/views/focus_class.dart';
 
 class SudokuHomePage extends StatefulWidget {
   const SudokuHomePage({Key? key}) : super(key: key);
@@ -9,9 +15,39 @@ class SudokuHomePage extends StatefulWidget {
 }
 
 class _SudokuHomePageState extends State<SudokuHomePage> {
+  List<BoxInner> boxInners = [];
+  FocusClass focusClass = FocusClass();
+  bool isFinish = false;
+  String? tapBoxIndex;
+
+  @override
+  void initState() {
+    generateSudoku();
+    super.initState();
+  }
+
+  void generateSudoku() {
+    isFinish = false;
+    focusClass = new FocusClass();
+    tapBoxIndex = null;
+    generatePuzzle();
+    checkFinish();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                generatePuzzle();
+              },
+              child: Icon(Icons.refresh))
+        ],
+      ),
       body: SafeArea(
           child: Container(
         alignment: Alignment.center,
@@ -27,32 +63,34 @@ class _SudokuHomePageState extends State<SudokuHomePage> {
               child: GridView.builder(
                   controller: ScrollController(),
                   shrinkWrap: true,
-                  itemCount: 9,
+                  itemCount: boxInners.length,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 3,
                       childAspectRatio: 1,
                       crossAxisSpacing: 5,
                       mainAxisSpacing: 5),
                   itemBuilder: (buildContext, index) {
+                    BoxInner boxInner =boxInners[index];
                     return Container(
                       color: Colors.red.shade100,
                       alignment: Alignment.center,
                       child: GridView.builder(
-                          controller: ScrollController(),
+                          physics: ScrollPhysics(),
                           shrinkWrap: true,
-                          itemCount: 9,
+                          itemCount: boxInner.blockChars.length,
                           gridDelegate:
                               SliverGridDelegateWithFixedCrossAxisCount(
                                   crossAxisCount: 3,
                                   childAspectRatio: 1,
                                   crossAxisSpacing: 2,
                                   mainAxisSpacing: 2),
-                          itemBuilder: (buildContext, index) {
+                          itemBuilder: (buildContext, indexChar) {
+                            BlockChar blockChar =boxInner.blockChars[indexChar];
                             return Container(
                               color: Colors.yellow.shade100,
                               alignment: Alignment.center,
                               child: Text(
-                                "${index + 1}",
+                                "${blockChar.text}",
                               ),
                             );
                           }),
@@ -68,7 +106,6 @@ class _SudokuHomePageState extends State<SudokuHomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Container(
-
                     child: GridView.builder(
                         physics: ScrollPhysics(),
                         scrollDirection: Axis.horizontal,
@@ -104,5 +141,56 @@ class _SudokuHomePageState extends State<SudokuHomePage> {
         ),
       )),
     );
+  }
+
+
+  generatePuzzle() {
+    // install plugins sudoku generator to generate one
+    boxInners.clear();
+    var sudokuGenerator = SudokuGenerator(emptySquares: 3); //54
+    // then we populate to get a possible cmbination
+    // Quiver for easy populate collection using partition
+    List<List<List<int>>> completes = partition(sudokuGenerator.newSudokuSolved,
+        sqrt(sudokuGenerator.newSudoku.length).toInt())
+        .toList();
+    partition(sudokuGenerator.newSudoku,
+        sqrt(sudokuGenerator.newSudoku.length).toInt())
+        .toList()
+        .asMap()
+        .entries
+        .forEach(
+          (entry) {
+        List<int> tempListCompletes =
+        completes[entry.key].expand((element) => element).toList();
+        List<int> tempList = entry.value.expand((element) => element).toList();
+
+        tempList.asMap().entries.forEach((entryIn) {
+          int index =
+              entry.key * sqrt(sudokuGenerator.newSudoku.length).toInt() +
+                  (entryIn.key % 9).toInt() ~/ 3;
+
+          if (boxInners.where((element) => element.index == index).length ==
+              0) {
+            boxInners.add(BoxInner(index, []));
+          }
+
+          BoxInner boxInner =
+              boxInners.where((element) => element.index == index).first;
+
+          boxInner.blockChars.add(BlockChar(
+            entryIn.value == 0 ? "" : entryIn.value.toString(),
+            index: boxInner.blockChars.length,
+            isDefault: entryIn.value != 0,
+            isCorrect: entryIn.value != 0,
+            correctText: tempListCompletes[entryIn.key].toString(),
+          ));
+        });
+      },
+    );
+
+    // complte generate puzzle sudoku
+  }
+  void checkFinish() {
+
   }
 }
