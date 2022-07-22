2aa5761cd5da6977167458321cbce1759ca27510
KasunHasanga <kasunhasanga96@gmail.com>
2022-07-22 18:39:31 +0530

    make bacis layout

--- lib/views/sudoku_home_page.dart
+++ lib/views/sudoku_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SudokuHomePage extends StatefulWidget {
   const SudokuHomePage({Key? key}) : super(key: key);
@@ -14,15 +15,17 @@ class _SudokuHomePageState extends State<SudokuHomePage> {
       body: SafeArea(
           child: Container(
         alignment: Alignment.center,
+        color: Colors.blueAccent,
         child: Column(
           children: [
             Container(
-              // color: Colors.red,
-              padding: EdgeInsets.all(20),
+              color: Colors.blueAccent.shade700,
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.all(5),
               width: double.maxFinite,
               alignment: Alignment.center,
               child: GridView.builder(
-                controller: ScrollController(),
+                  controller: ScrollController(),
                   shrinkWrap: true,
                   itemCount: 9,
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -30,19 +33,72 @@ class _SudokuHomePageState extends State<SudokuHomePage> {
                       childAspectRatio: 1,
                       crossAxisSpacing: 5,
                       mainAxisSpacing: 5),
-                  itemBuilder: (buildContext,index){
+                  itemBuilder: (buildContext, index) {
                     return Container(
-                      color: Colors.red,
+                      color: Colors.red.shade100,
                       alignment: Alignment.center,
-                      child: Text("${index + 1}",),
+                      child: GridView.builder(
+                          controller: ScrollController(),
+                          shrinkWrap: true,
+                          itemCount: 9,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 3,
+                                  childAspectRatio: 1,
+                                  crossAxisSpacing: 2,
+                                  mainAxisSpacing: 2),
+                          itemBuilder: (buildContext, index) {
+                            return Container(
+                              color: Colors.yellow.shade100,
+                              alignment: Alignment.center,
+                              child: Text(
+                                "${index + 1}",
+                              ),
+                            );
+                          }),
                     );
                   }),
             ),
             Expanded(
                 child: Container(
+              padding: EdgeInsets.all(20),
               alignment: Alignment.center,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
 
-              child: Text("action"),
+                    child: GridView.builder(
+                        physics: ScrollPhysics(),
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemCount: 9,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            childAspectRatio: 1,
+                            crossAxisSpacing: 5,
+                            mainAxisSpacing: 5),
+                        itemBuilder: (buildContext, index) {
+                          return ElevatedButton(
+                            onPressed: () {},
+                            child: Text(
+                              "${index + 1}",
+                            ),
+                          );
+                        }),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10),
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("Clear"),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ))
           ],
         ),
